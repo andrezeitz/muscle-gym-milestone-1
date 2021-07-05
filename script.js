e5fe42d25e0890ef3100b2842140b8b352518d39
andrezeitz <swe_zeitz@hotmail.com>
2021-07-05 10:20:06 +0000

    Style nav-bar to be a fixed top menu and add margins to each section that have a nav-link

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// I learned how to make the nav-bar icons to smooth scroll from this tutorial from W3Schools
+
 const toggleButton = document.getElementsByClassName('toggle-button')[0]
 const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 
