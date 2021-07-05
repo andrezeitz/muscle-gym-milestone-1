1c6b21d3319397122a74cf9f9ba419deff11198f
andrezeitz <swe_zeitz@hotmail.com>
2021-07-05 15:05:35 +0000

    Change javascript code to make the list dissapear after clicking on a section

--- assets/script/script.js
+++ assets/script/script.js
@@ -2,7 +2,15 @@
 
 const toggleButton = document.getElementsByClassName('toggle-button')[0]
 const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+const allNavbarLinks = document.getElementsByClassName('nav-link')
 
 toggleButton.addEventListener('click', () => {
   navbarLinks.classList.toggle('active')
 })
+
+for (let i = 0; i < allNavbarLinks.length; i++) {
+  allNavbarLinks[i].addEventListener("click", () => {
+  console.log("clicked");
+  toggleButton.click();
+  })
+}
